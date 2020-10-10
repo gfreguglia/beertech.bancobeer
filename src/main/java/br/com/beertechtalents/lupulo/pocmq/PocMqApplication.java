930c99a9a250ba7dbfa741c7847675863ee80838
Rodrigo Siqueira <rodrigo.siqueira@mv.com.br>
2020-10-09 21:22:26 -0300

    First commit

---
+++ src/main/java/br/com/beertechtalents/lupulo/pocmq/PocMqApplication.java
@@ -0,0 +1,13 @@
+package br.com.beertechtalents.lupulo.pocmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/beertechtalents/lupulo/pocmq/PocMqApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.beertechtalents.lupulo.pocmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
