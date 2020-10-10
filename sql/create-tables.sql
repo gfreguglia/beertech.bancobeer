179ff620ebe807a21643852a56bbf1e3b9dea8f0
Rodrigo Siqueira <rodrigo.siqueira@mv.com.br>
2020-10-10 13:52:42 -0300

    Prevent bug on insert and automatic queue creation

--- sql/create-tables.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `transacoes`;
-
-CREATE TABLE `transacoes` (
-  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
-  `datahora` datetime NOT NULL,
-  `valor` decimal(15,2) NOT NULL,
-  `tipo` varchar(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
