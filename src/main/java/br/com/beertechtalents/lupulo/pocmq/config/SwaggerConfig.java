63c3cb1a1e637a4499c453d4e4d008f5107e3fd5
Rodrigo Siqueira <rodrigo.siqueira@mv.com.br>
2020-10-10 10:29:48 -0300

    Configure SwaggerUI

--- src/main/java/br/com/beertechtalents/lupulo/pocmq/config/SwaggerConfig.java
+++ src/main/java/br/com/beertechtalents/lupulo/pocmq/config/SwaggerConfig.java
@@ -1,4 +1,23 @@
 package br.com.beertechtalents.lupulo.pocmq.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("br.com.beertechtalents.lupulo"))
+                .build();
+    }
 }
