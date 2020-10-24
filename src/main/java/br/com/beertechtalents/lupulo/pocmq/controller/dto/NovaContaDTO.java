eb3a499df27965bc7460acf7d4ffe773902926ae
Gabriel Freguglia <gabriel.batata@gmail.com>
2020-10-24 10:45:14 -0300

    Add empty field validation on account creation

--- src/main/java/br/com/beertechtalents/lupulo/pocmq/controller/dto/NovaContaDTO.java
+++ src/main/java/br/com/beertechtalents/lupulo/pocmq/controller/dto/NovaContaDTO.java
@@ -1,11 +1,22 @@
 package br.com.beertechtalents.lupulo.pocmq.controller.dto;
 
 import lombok.Data;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
+@Validated
 public class NovaContaDTO {
+    @NotBlank
     String nome;
+
+    @NotBlank
     String email;
+
+    @NotBlank
     String cnpj;
+
+    @NotBlank
     String senha;
 }
