eb5535ce115472da2fbaf20b42e7c718ee3e9dca
Rodrigo Siqueira <rodrigo.siqueira@mv.com.br>
2020-10-25 23:04:43 -0300

    Fix Controller tests

--- src/test/java/br/com/beertechtalents/lupulo/pocmq/controller/TransferenciaControllerTest.java
+++ src/test/java/br/com/beertechtalents/lupulo/pocmq/controller/TransferenciaControllerTest.java
@@ -6,30 +6,37 @@ import br.com.beertechtalents.lupulo.pocmq.model.Operacao;
 import br.com.beertechtalents.lupulo.pocmq.repository.ContaRepository;
 import br.com.beertechtalents.lupulo.pocmq.service.ContaService;
 import br.com.beertechtalents.lupulo.pocmq.service.OperacaoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.*;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TransferenciaControllerTest {
 
-    @LocalServerPort
-    private int port;
-
     @Autowired
-    private TestRestTemplate restTemplate;
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
 
     @Autowired
     ContaService contaService;
@@ -40,6 +47,9 @@ class TransferenciaControllerTest {
     @Autowired
     OperacaoService operacaoService;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
     Conta conta1 = new Conta();
     Conta conta2 = new Conta();
 
@@ -48,63 +58,79 @@ class TransferenciaControllerTest {
 
         Operacao operacao = new Operacao();
         conta1.setNome("CONTA");
+        conta1.setEmail("teste@teste.com");
+        conta1.setCnpj("11111111111111");
+        conta1.setSenha(passwordEncoder.encode("123"));
         conta1 = contaService.salvar(conta1);
         operacao.setConta(conta1);
         operacao.setTipo(Operacao.TipoTransacao.DEPOSITO);
         operacao.setValor(BigDecimal.valueOf(100));
+        operacao.setDescricaoOperacao(Operacao.DescricaoOperacao.DEPOSITO);
         operacaoService.salvarOperacao(operacao);
 
-        conta2.setNome("CONTA");
+        conta2.setNome("CONTA2");
+        conta2.setEmail("teste2@teste.com");
+        conta2.setCnpj("11111111111112");
+        conta2.setSenha(passwordEncoder.encode("123"));
         conta2 = contaService.salvar(conta2);
         operacao = new Operacao();
         operacao.setConta(conta2);
         operacao.setTipo(Operacao.TipoTransacao.DEPOSITO);
         operacao.setValor(BigDecimal.valueOf(100));
+        operacao.setDescricaoOperacao(Operacao.DescricaoOperacao.DEPOSITO);
+        operacaoService.salvarOperacao(operacao);
+
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
     }
 
     @Test
-    void transferShouldReturnSuccess() {
+    void transferShouldReturnSuccess() throws Exception {
 
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost")
-                .port(port)
-                .path("/transferencia");
+        SecurityContextHolder.getContext()
+                .setAuthentication(new PreAuthenticatedAuthenticationToken(null,
+                        null,
+                        Arrays.asList(new SimpleGrantedAuthority(conta1.getUuid().toString()))));
 
         NovaTransferenciaDTO dto = new NovaTransferenciaDTO();
         dto.setOrigem(conta1.getUuid());
         dto.setDestino(conta2.getUuid());
         dto.setValor(new BigDecimal("50.00"));
 
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity entity = new HttpEntity(dto, httpHeaders);
-
-        ResponseEntity<?> responseEntity = this.restTemplate.exchange(builder.toUriString(), HttpMethod.POST, entity, String.class);
-        assertThat(responseEntity.getStatusCode().is2xxSuccessful()).isTrue();
+        ObjectMapper mapper = new ObjectMapper();
+        mvc.perform(post("/transferencia")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(dto)))
+                .andExpect(status().isNoContent());
     }
 
     @Test
-    void transferOnUnknownOriginAccountShouldReturnError() {
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost")
-                .port(port)
-                .path("/transferencia")
-                .queryParam("origem", UUID.randomUUID())
-                .queryParam("destino", conta2.getUuid())
-                .queryParam("valor", 100);
+    void transferOnUnknownOriginAccountShouldReturnError() throws Exception {
+        UUID random = UUID.randomUUID();
+        SecurityContextHolder.getContext()
+                .setAuthentication(new PreAuthenticatedAuthenticationToken(null,
+                        null,
+                        Arrays.asList(new SimpleGrantedAuthority(random.toString()))));
 
-        ResponseEntity<?> responseEntity = this.restTemplate.exchange(builder.toUriString(), HttpMethod.POST, null, String.class);
-        assertThat(responseEntity.getStatusCode().is4xxClientError()).isTrue();
+        mvc.perform(post("/transferencia",
+                "origem", random,
+                "destino", conta2.getUuid(),
+                "valor", 100))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
-    void transferOnUnknownDestinationAccountShouldReturnError() {
-        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl("http://localhost")
-                .port(port)
-                .path("/transferencia")
-                .queryParam("origem", conta1.getUuid())
-                .queryParam("destino", UUID.randomUUID())
-                .queryParam("valor", 100);
+    void transferOnUnknownDestinationAccountShouldReturnError() throws Exception {
+        SecurityContextHolder.getContext()
+                .setAuthentication(new PreAuthenticatedAuthenticationToken(null,
+                        null,
+                        Arrays.asList(new SimpleGrantedAuthority(conta1.getUuid().toString()))));
 
-        ResponseEntity<?> responseEntity = this.restTemplate.exchange(builder.toUriString(), HttpMethod.POST, null, String.class);
-        assertThat(responseEntity.getStatusCode().is4xxClientError()).isTrue();
+        mvc.perform(post("/transferencia",
+                "origem", conta1.getUuid(),
+                "destino", UUID.randomUUID(),
+                "valor", 100))
+                .andExpect(status().isBadRequest());
     }
 }
